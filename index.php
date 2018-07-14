0d753b24955924c96686a34c9bf10be16b6d0c0b
root <root@ip-172-31-22-246.ap-south-1.compute.internal>
2018-07-14 12:04:55 +0000

    final changes

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require_once 'HomeController.php';
 require_once 'CachingController.php';
+require_once 'ValidateController.php';
 require 'vendor/autoload.php';
 
 $container = new \Slim\Container;
@@ -22,64 +23,57 @@ $container['cache'] = function ($c) {
     return new Symfony\Component\Cache\Adapter\RedisAdapter($connection);
 };
 
-$app->get('/foo', function (Request $request, Response $response, array $args){
-    $bar = new CachingController($this->get('cache'));
-    return $bar->doSomething();
+# handle default condition
+$app->get('/', function (Request $request, Response $response, array $args){
+	$data = array("status" => "error", "message" => "you are on wrong page", "data"  => [], "code" => 404);
+        $response = $response->withHeader('Content-Type', 'application/json');
+        $response = $response->withStatus(404);
+        $response = $response->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT));
+        return $response;
 });
 
 
-$app->get('/too', function (Request $request, Response $response, array $args){
+$app->get('/top-headlines', function (Request $request, Response $response, array $args){
+    # validate input data
+    $validate = new ValidateController();
+    $validator = $validate->validate($request);
+    if ($validator['status'] == "error") {
+	$data = array("status" => "error", "message" => $validator['message'], "data"  => $output, "code" => 400);
+        $response = $response->withHeader('Content-Type', 'application/json');
+        $response = $response->withStatus(400);
+        $response = $response->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT));
+        return $response;
+    }
+    # check in cache (if request is made within 10 minutes, return same data)
+    $cache = new CachingController($this->get('cache'));
+    $output = $cache->check($validator['data']);
+    #return $bar->doSomething();
+    if (!empty($output)) {
+    	$data = array("status" => "success", "message" => "Thank you for the request, this is being served from cache", "data"  => $output,  "code" => 201);
+    	$response = $response->withHeader('Content-Type', 'application/json');
+    	$response = $response->withStatus(200);
+    	$response = $response->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT));
+    	return $response;
+    }
+
+    # process the request
     $bar = new HomeController();
-    return $bar->home();
-});
-
-
-$app->get('/top-headlines', function (Request $request, Response $response, array $args) { 
-	//print_r($request);	
-	$body = $request->getQueryParams();
-
-	#hit curl
-	$url = 'https://newsapi.org/v2/top-headlines?country=' . strtolower($body['country']) . '&category=' . strtolower($body['category']) . '&apiKey=eadb6da4bb5847a8b5f5b8a633e53ab9';
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_HEADER, 0);            // No header in the result 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return, do not echo result   
-
-	// Fetch and return content, save it.
-	$raw_data = curl_exec($ch);
-	curl_close($ch);
-
-	// If the API is JSON, use json_decode.
-	$res_data = json_decode($raw_data, true);
-	#frame our data
-	foreach($res_data['articles'] as $article) {
-		$output[] = array('News Title' => $article['title'], 'Description' => $article['description'], 'Source News Url' => $article['url'], 'Country' => $body['country'], 'Category' => $body['category'], 'Filter Keyword' => empty($body['keyword']) ? "" : $body['keyword']);
-	}	
-
-	$data = array("status" => "success", "message" => "Thank you for the request", "data"  => $output, "body" => $body,  "code" => 200);
-	$response = $response->withHeader('Content-Type', 'application/json');
-	$response = $response->withStatus(200);
-	$response = $response->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT));
-	return $response;
-
-});
-
-/*$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    $response->getBody()->write("\n Thank you for requesting top-headlines");
-
+    $output = $bar->home($request, $response, $args, $validator['data']);
+    
+    if (!empty($output)) {
+    	# add data to cache
+    	$cache->add($validator['data'], $output);
+    	# return output
+    	$data = array("status" => "success", "message" => "Thank you for the request", "data"  => $output, "code" => 200);
+    } else {
+	$data = array("status" => "success", "message" => "No articles available for this request", "data"  => $output, "code" => 404);
+    }
+    $response = $response->withHeader('Content-Type', 'application/json');
+    $response = $response->withStatus(200);
+    $response = $response->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT));
     return $response;
-});*/
+});
+
 $app->run();
 
-
-/*require 'vendor/autoload.php';
-$app = new Slim\App();
-
-$app->get('/top-headlines', function() {
-	echo "thank you for requesting top-headlines";
-});
-
-$app->run();*/
 ?>
